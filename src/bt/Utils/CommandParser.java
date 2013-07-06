8a4bcf4b505745d9dccaccfaa5a0df9074bfffb7
Robert Schomburg <Robert.Schomburg@rutgers.edu>
2013-07-06 05:09:04 -0400

    Peer handshake generation
    No more compile errors, but not yet ready to transmit handshake message.

---
+++ src/bt/Utils/CommandParser.java
@@ -0,0 +1,39 @@
+package bt.Utils;
+
+/**
+ * It will statically handle commands and parse them while the client runs.
+ * @author Yke, Robert and Fernando
+ *
+ */
+
+public class CommandParser {
+	
+	/** 
+	 * Main entry point for parsing commands.
+	 * @param String command
+	 */
+	public static void execute(String command) {
+		switch(command) {
+		case "run":
+//			bt.Model.Peer.run();
+			break;
+		case "quit":
+			Utilities.callClose();
+		case "help":
+			CommandParser.printHelp();
+			break;
+		default:
+			throw new IllegalArgumentException("Invalid command, input help for commands.");
+		}
+	}
+	
+	/**
+	 * Prints a list of available commands. It will expand as we advance.
+	 */
+	public static void printHelp() {
+		System.out.println("\nHELP - Available Commands (so far):");
+		System.out.println("quit\t - terminates the client.");
+		System.out.println("--------------------------------\n");
+	}
+	
+}
