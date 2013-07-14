23002771e17b2f15950f3f9ccb062e7430c9e2a1
Isaac <isaac.yochelson@rutgers.edu>
2013-07-14 14:02:06 -0400

    replaced getStackTrace

--- src/bt/Model/Peer.java
+++ src/bt/Model/Peer.java
@@ -311,11 +311,11 @@ public class Peer implements Runnable {
 			}
 			try {
 				Bittorrent.getInstance().addBytesToPiece(index, offset);
-			} catch (Exception e) {System.out.println(e.getStackTrace());}
+			} catch (Exception e) {System.out.println(e.getMessage());}
 			try {
 				verifySHA(index);
 			} catch (Exception e) {
-				System.err.println(e.getStackTrace());
+				System.err.println(e.getMessage());
 			}
 		}
 	}
@@ -541,7 +541,7 @@ public class Peer implements Runnable {
 				}
 			} 
 		} catch (NoSuchAlgorithmException e) {
-			System.err.println(e.getStackTrace());
+			System.err.println(e.getMessage());
 		}
 	}
 	
